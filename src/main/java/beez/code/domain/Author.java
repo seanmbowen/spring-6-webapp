1cbaba43fc6020f13d25bc6ae9d82157ff99e97a
Sean M. Bowen <sean.bowen@fullstackit.ca>
2024-02-19 16:00:30 -0500

    Add Book and Author entities
    Two new entity classes, Book and Author, have been added to the domain package. These classes include necessary annotations for persistence and a ManyToMany relationship between them. Both entities have basic attributes and getters/setters.

---
+++ src/main/java/beez/code/domain/Author.java
@@ -0,0 +1,33 @@
+package beez.code.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+/**
+ * Created by: seanbowen on 2024-02-19
+ * Architecting Solutions. Simply.
+ *
+ * @author seanbowen
+ * @version 1.0
+ * @since 2024-02-19
+ */
+@Entity
+@Getter
+@Setter
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+}
---
+++ src/main/java/beez/code/domain/Book.java
@@ -0,0 +1,36 @@
+package beez.code.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+/**
+ * Created by: seanbowen on 2024-02-19
+ * Architecting Solutions. Simply.
+ *
+ * @author seanbowen
+ * @version 1.0
+ * @since 2024-02-19
+ */
+@Entity
+@Getter
+@Setter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+}
